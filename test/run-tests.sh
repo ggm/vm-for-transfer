1b4b0fc8d4f1454107f214f09828308a50ac39e6
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-06-09 11:37:22 +0200

    Just a simple script to run the tests and check if a change breaks anything.

---
+++ test/run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+input=test/data/
+output=test/expected_output/
+
+echo "============================================"
+echo "* Running tests..."
+echo "* input = $input"
+echo "* expected_output = $output"
+echo "============================================"
+
+for arg in `ls $output` ; do
+  python3 src/compiler/ -i $input$arg.t1x -d compiler.log > compiler.out
+  if diff compiler.out $output$arg > test_results.log ; then
+    echo $arg "-- OK"
+  else
+    echo $arg "-- Error"
+    cat test_results.log
+    fi
+done
+
+rm compiler.log compiler.out test_results.log
