2ae84d3c84c26df9fe6c9f9174668851721f5545
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-06-10 12:02:57 +0200

    Added error checking to the part attribute of a <clip> element.

--- src/compiler/eventhandler.py
+++ src/compiler/eventhandler.py
@@ -259,7 +259,9 @@ class EventHandler():
         else:
             linkTo = False
             part = event.attrs['part']
-            if part in "lem lemh lemq whole": partAttrs.append(part)
+            if part not in self.defAttrs:
+                if part in "lem lemh lemq whole": partAttrs.append(part)
+                else: self.raiseError("attr '{}' doesn't exist.".format(part), event)
             else: partAttrs = self.defAttrs[part]
 
         isContainer = False
