574b44d1964fac8f6d4af61ccc59349e5cdb78b8
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-07-05 19:14:29 +0200

    Implemented and added tests for proper handling of patterns in the trie:
      - Patterns which start directly with tags (should accept any lemma, e.g.
        <n><pl> -> should accept student<n><pl>).
    
      - Patterns which contain *, e.g. <n><*><sg><*><gen>.

--- src/vm/systemtrie.py
+++ src/vm/systemtrie.py
@@ -35,32 +35,88 @@ class SystemTrie:
     def __init__(self):
         self.root = TrieNode()
 
-    def getPatternNode(self, pattern, startNode=None):
-        """Get the last node of the sequence representing the pattern."""
+    def __getNextNodes(self, char, startNode):
+        """Get a list of next nodes given a char and a start node."""
+
+        nextNodes = []
+        if char.isalpha() and '*' in startNode.children:
+            nextNodes.append(startNode.children['*'])
+
+        try:
+            nextNodes.append(startNode.children[char])
+        except KeyError:
+            pass
+
+        return nextNodes
+
+    def __insertPattern(self, pattern, ruleNumber=None, node=None):
+        """Internal method used only by this class to insert part of a
+           pattern, starting in a node passed as argument.
+        """
+
+        if not node: curNode = self.root
+        else: curNode = node
+
+        for char in pattern:
+            if char == '*': curNode = self.__insertTagStar(curNode)
+            else: curNode = curNode.children.setdefault(char, TrieNode())
+        curNode.ruleNumber = ruleNumber
+
+        return curNode
+
+    def __insertTagStar(self, node):
+        """Insert a <*> to get a series of tags."""
+
+        #Insert the star node following the last one.
+        node = node.children.setdefault('*', TrieNode())
+        #Add a reference to itself to accept every alphabet char.
+        node.addChild('*', node)
+        starNode = node
+        #Add the end and the start of a possible next tag.
+        node = node.children.setdefault('>', TrieNode())
+        node = node.children.setdefault('<', TrieNode())
+        #Add the reference to the star node and the cycle is complete.
+        node.addChild('*', starNode)
+
+        return starNode
+
+    def getPatternNodes(self, pattern, startNode=None):
+        """Get the last nodes of the sequence representing the pattern."""
 
         if not pattern: return None
 
-        if startNode: curNode = startNode
-        else: curNode = self.root
+        if startNode: curNodes = [startNode]
+        else: curNodes = [self.root]
 
         for char in pattern:
-            try:
-                curNode = curNode.children[char]
-            except KeyError:
-                return None
+            nextNodes = []
+            for node in curNodes:
+                nextNodes.extend(self.__getNextNodes(char, node))
+            curNodes = nextNodes
+            if len(curNodes) == 0: return None
 
-        return curNode
+        return curNodes
 
     def getRuleNumber(self, pattern):
         """Get the rule number for a given pattern."""
 
-        curNode = self.getPatternNode(pattern)
+        curNodes = self.getPatternNodes(pattern)
 
-        if curNode: return curNode.ruleNumber
+        if curNodes:
+            try:
+                #If there are several possible rules, return the first which
+                #appears on the rules file.
+                return min(node.ruleNumber
+                                for node in curNodes if node.ruleNumber)
+            except ValueError: return None
         else: return None
 
     def addPattern(self, pattern, ruleNumber):
-        #One string will be processed by character and a list by part.
+        """Add a pattern, as a string or a list of strings, to the trie."""
+
+        #Convert the pattern to a list if it's just a string.
+        if not isinstance(pattern, list): pattern = [pattern]
+
         #Firstly we add all the parts of the pattern but the last one. Options
         #are stored in a list of curNodes and the next part is inserted after
         #each option, for example, given the input:
@@ -75,38 +131,38 @@ class SystemTrie:
         #                           |
         #                            \~~<comp>~~<adv_pp> -> 12
         #
+        # Note: if a pattern starts directly with tags, we need to add a node
+        # for every alphabetic char (*), so it can accept patterns with lemmas.
+        
         curNodes = [self.root]
         for part in pattern[:-1]:
             lastNodes = []
             for node in curNodes:
                 if '|' in part:
                     for option in part.split('|'):
-                        lastNodes.append(self._insertPattern(option, node=node))
+                        if option[0] == '<': node.addChild('*', node)
+                        lastNodes.append(self.__insertPattern(option, node=node))
                 else:
-                    lastNodes.append(self._insertPattern(part, node=node))
+                    if part[0] == '<': node.addChild('*', node)
+                    lastNodes.append(self.__insertPattern(part, node=node))
             curNodes = lastNodes
 
         #Finally, we add the last part of the pattern with its rule number.
         lastPart = pattern[-1]
         for node in curNodes:
-            self._insertPattern(lastPart, ruleNumber, node)
-
-    def _insertPattern(self, pattern, ruleNumber=None, node=None):
-        """Internal method used only by this class to insert part of a
-           pattern, starting in a node passed as argument.
-        """
-
-        if not node: curNode = self.root
-        else: curNode = node
-
-        for char in pattern:
-            curNode = curNode.children.setdefault(char, TrieNode())
-        curNode.ruleNumber = ruleNumber
-
-        return curNode
+            if lastPart[0] == '<': node.addChild('*', node)
+            self.__insertPattern(lastPart, ruleNumber, node)
 
 if __name__ == '__main__':
-    #Some tests for reference.
+
+    def testPattern(key, expected):
+        actual = trie.getRuleNumber(key)
+        if actual != expected:
+            print("Error: {} is {}, not {}".format(key, actual, expected))
+        else:
+            print("Test Ok: {} is {}".format(key, actual))
+
+    #Some tests for reference purposes.
     trie = SystemTrie()
 
     testDict = {"<det><nom><adj>" : 11, "<det><adj><nom>" : 12,
@@ -115,28 +171,20 @@ if __name__ == '__main__':
                 "<nom><adj><adj>" : 23,
                 "<adv_pp>" : 31, "<adv_pp><adj><adj>" : 32
                 }
-
     #Add all the patterns with their rule's number.
     for key, value in testDict.items():
         trie.addPattern(key, value)
-
     #Test the retrieval of the trie.
     for key, expected in testDict.items():
-        actual = trie.getRuleNumber(key)
-        if actual != expected:
-            print("Error: {} is {}, not {}".format(key, actual, expected))
-        else:
-            print("Test Ok: {} is {}".format(key, actual))
+        testPattern(key, expected)
 
     #Test when there isn't a pattern.
-    actual = trie.getRuleNumber("<nonexsitent>")
-    if not actual:
-        print("Test Ok: nonexistent pattern")
-    else:
-        print("Error: nonexistent pattern exists as {}".format(actual))
+    testPattern("<nonexistent>", None)
 
     #Test the option parts.
-    pattern = ["<det><nom>", "<adj>|<adj><sint>|<adj><comp>|<adj><sup>", "<adv_pp>"]
+    pattern = ["<det><nom>",
+               "<adj>|<adj><sint>|<adj><comp>|<adj><sup>",
+               "<adv_pp>"]
     trie.addPattern(pattern, 40)
 
     testOptions = {"<det><nom><adj><adv_pp>" : 40,
@@ -144,10 +192,23 @@ if __name__ == '__main__':
                    "<det><nom><adj><comp><adv_pp>" : 40,
                    "<det><nom><adj><sup><adv_pp>" : 40
                    }
-
     for key, expected in testOptions.items():
-        actual = trie.getRuleNumber(key)
-        if actual != expected:
-            print("Error: {} is {}, not {}".format(key, actual, expected))
-        else:
-            print("Test Ok: {} is {}".format(key, actual))
+        testPattern(key, expected)
+
+    #Test that a pattern starting with tag is inserted as *<pattern>.
+    patterns = ["all<predet><sp><n><pl>", "all<predet><sp>student<n><pl>"]
+    expected = 50
+    trie.addPattern(["all<predet><sp>", "<n><pl>"], expected)
+    for key in patterns:
+        testPattern(key, expected)
+
+    #Test * inside tags.
+    patterns = {"<n><sg><gen>" : None, "<n><m><sg><gen>" : None,
+                "<n><m><sg><ND><gen>" : 55, "student<n><sp><sg><f><gen>" : 55,
+                "<vblex>" : None, "<vblex><pp>" : 56, "<vblex><pron><pp>" : 56,
+                "<pp><vblex>" : None, "<vblex><sep><past><pron><pp>" : 56
+                }
+    trie.addPattern("<n><*><sg><*><gen>", 55)
+    trie.addPattern("<vblex><*>", 56)
+    for key, expected in patterns.items():
+        testPattern(key, expected)
