fbf5bb6cb764624aa5cf301e8858e0e48ee46aed
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-07-08 10:48:00 +0200

    Parse all the attributes of a lexical unit.

--- src/vm/transferword.py
+++ src/vm/transferword.py
@@ -19,10 +19,13 @@ class TransferWord:
 
     def __init__(self):
         self.source = ""
+        self.sourceAttrs = {}
         self.target = ""
+        self.targetAttrs = {}
 
     def __str__(self):
-        return "^{}/{}$".format(self.source, self.target)
+        return "^{}/{}$: {}/{}".format(self.source, self.target,
+                                       self.sourceAttrs, self.targetAttrs)
 
 class TransferWordTokenizer():
     """Create a set of transfer words from an input stream."""
@@ -36,18 +39,47 @@ class TransferWordTokenizer():
         input = input.read()
         tokens = []
         token = ""
+        firstTag = True
+        source = True
 
         word = TransferWord()
         for char in input:
             if char == '^': pass
             elif char == '$':
                 word.target = token.strip()
+                #Set the queue and tags attributes.
+                tag = word.target.find('<')
+                head = word.target.find('#')
+                if head > -1: word.targetAttrs['queue'] = word.target[head:tag]
+                word.targetAttrs['tags'] = word.target[tag:]
                 tokens.append(word)
+                #Initialize auxiliary variables.
+                source = True
+                firstTag = True
                 token = ""
                 word = TransferWord()
             elif char == '/':
                 word.source = token.strip()
+                #Set the queue and tags attributes.
+                head = word.source.find('#')
+                tag = word.source.find('<')
+                if head > -1: word.sourceAttrs['queue'] = word.source[head:tag]
+                word.sourceAttrs['tags'] = word.source[tag:]
+                #Initialize auxiliary variables.
+                source = False
+                firstTag = True
                 token = ""
+            elif firstTag and char == '<' :
+                #The lemma is everything until the first tag.
+                if source: word.sourceAttrs['lem'] = token.strip()
+                else: word.targetAttrs['lem'] = token.strip()
+                token += str(char)
+                firstTag = False
+            elif char == '#':
+                #The head is the part of the lemma until the '#' character.
+                if source: word.sourceAttrs['head'] = token.strip()
+                else: word.targetAttrs['head'] = token.strip()
+                token += str(char)
             else: token += str(char)
 
         return tokens
