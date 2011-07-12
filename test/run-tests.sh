4b078314585ffda94f74f356231c5d058f40c7fc
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-07-12 10:25:07 +0200

    Fixed parameter substitution for the new tests.

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -9,12 +9,16 @@ echo "* expected_output = $output"
 echo "============================================"
 
 for arg in `ls $output` ; do
-  python3 src/compiler/ -i $input${arg/.v?x/.t?x} -d compiler.log > compiler.out
+  inputarg=$input$arg
+  inputarg=${inputarg/.v1x/.t1x}
+  inputarg=${inputarg/.v2x/.t2x}
+  inputarg=${inputarg/.v3x/.t3x}
+  python3 src/compiler/ -i $inputarg -d compiler.log > compiler.out
   if diff compiler.out $output$arg > test_results.log ; then
     echo $arg "-- OK"
   else
     echo $arg "-- Error"
-    cat test_results.log
+    #cat test_results.log
     fi
 done
 
