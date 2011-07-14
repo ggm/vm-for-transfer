7452a81854713e649cb24bc82f734ca1306a0627
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-07-14 12:04:34 +0200

    Added more functionality to the debugger (break, continue, delete, info, print, run and step). Only list left to implement.

--- src/vm/debugger.py
+++ src/vm/debugger.py
@@ -23,6 +23,8 @@ class Debugger:
         self.interpreter = None
         self.breakpoints = []
         self.breakOnRule = False
+        self.step = False
+        self.lastCommand = ""
 
         #Components used by the debugger.
         self.vm = vm
@@ -43,17 +45,30 @@ class Debugger:
     def getCommand(self):
         """Get the user command and execute its appropiate method."""
 
-        command = input("(vmdb) ")
-        command = command.strip().split(' ')
+        try:
+            command = input("(vmdb) ")
 
-        cmd = command[0]
-        args = command[1:]
-        if cmd in ('h', 'help'): self.commandHelp(args)
-        elif cmd in ('q', 'quit'): self.commandQuit()
-        elif cmd in ('r', 'run'): self.commandRun()
-        else:
-            print("unknown command: '{}'. Try 'help'.".format(cmd))
-            self.getCommand()
+            if command == "": command = self.lastCommand
+            else: self.lastCommand = command
+
+            command = command.strip().split(' ')
+            cmd = command[0]
+            args = command[1:]
+            if cmd in ('h', 'help'): self.commandHelp(args)
+            elif cmd in ('q', 'quit'): self.commandQuit()
+            elif cmd in ('br', 'break'): self.commandBreak(args)
+            elif cmd in ('c', 'continue'): self.commandContinue()
+            elif cmd in ('d', 'delete'): self.commandDelete(args)
+            elif cmd in ('i', 'info'): self.commandInfo(args)
+            elif cmd in ('l', 'list'): self.commandList(args)
+            elif cmd in ('p', 'print'): self.commandPrint(args)
+            elif cmd in ('r', 'run'): self.commandRun()
+            elif cmd in ('s', 'step'): self.commandStep()
+            else:
+                print("unknown command: '{}'. Try 'help'.".format(cmd))
+                self.getCommand()
+        except KeyboardInterrupt:
+            self.commandQuit()
 
     def commandHelp(self, args):
         if len(args) == 0: self.showDebuggerHelp()
@@ -64,13 +79,81 @@ class Debugger:
     def commandQuit(self):
         self.vm.status = VM_STATUS.HALTED
 
+    def commandBreak(self, args):
+        if len(args) == 0 or args[0] == "onrule":
+            self.breakOnRule = True
+            print("Set breakpoint by default when a new rule starts.")
+        elif args[0].isnumeric():
+            codeLine = args[0]
+            self.breakpoints.append(int(codeLine))
+            print("Set breakpoint at {} line.".format(codeLine))
+
+        self.getCommand()
+
+    def commandContinue(self):
+        pass
+
+    def commandDelete(self, args):
+        if len(args) == 0:
+            answer = ''
+            while answer != 'y' and answer != 'n':
+                answer = input("Do you want to delete all breakpoints? (y o n) ")
+            if answer == 'y':
+                self.breakOnRule = False
+                self.breakpoints = []
+        elif args[0].isnumeric():
+            breakPos = int(args[0])
+            del self.breakpoints[breakPos]
+        elif args[0] == "onrule": self.breakOnRule = False
+
+        self.getCommand()
+
+    def commandInfo(self, args):
+        if len(args) == 0: pass
+        elif args[0] in ('br', 'breakpoints'):
+            if self.breakOnRule: print("Breakpoint on rule start")
+            if len(self.breakpoints) > 0:
+                print("{}\t {}".format("Num", "Line"))
+                for n, br in enumerate(self.breakpoints):
+                    print("{}\t {}".format(n, br))
+        elif args[0] in ('st', 'stack'):
+            print(self.vm.stack)
+
+        self.getCommand()
+
+    def commandList(self, args):
+        pass
+        self.getCommand()
+
+    def commandPrint(self, args):
+        if len(args) == 0: pass
+        elif args[0] == "variables":
+            print(self.vm.variables)
+        else:
+            varName = args[0]
+            try:
+                print("{} = '{}'".format(varName, self.vm.variables[varName]))
+            except KeyError:
+                print("Variable '{}' doesn't exists, maybe it hasn't been "
+                      "initialized".format(varName))
+
+        self.getCommand()
+
     def commandRun(self):
         pass
 
+    def commandStep(self):
+        self.step = True
+
     def showDebuggerHelp(self):
         print("List of commands available (abbreviation and full command):")
+        print()
         print("br, break: to set a breakpoint")
+        print("c, continue: continue execution until a breakpoint or the end")
+        print("d, delete: delete all breakpoints or the indicated one")
         print("i, info: show some info about the vm and its current state")
+        print("l, list: list code ten lines by ten lines")
+        print("p, print: print the value of a variable")
         print("r, run: execute the current loaded program")
         print("s, step: execute the next line and stop")
         print("h, help: show this help")
@@ -82,16 +165,55 @@ class Debugger:
     def showCommandHelp(self, cmd):
         if cmd in ('h', 'help'): self.showDebuggerHelp()
         elif cmd in ('q', 'quit'): print("q, quit: quit the debugger")
+        elif cmd in ('br', 'break'):
+            print("break [N | onrule]")
+            print("\t 'break' by default will set a breakpoint on rule start")
+            print("\t 'break N' will set a breakpoint at line N")
+            print("\t 'break onrule will set a breakpoint at rule start")
+        elif cmd in ('c', 'continue'):
+            print("c, continue: continue execution until a breakpoint or the end")
+        elif cmd in ('d', 'delete'):
+            print("delete [N | onrule]:")
+            print("\t 'delete' will delete all current breakpoints")
+            print("\t 'delete N' will delete the number N breakpoint")
+            print("\t 'delete onrule' will delete the breakpoint on rule start")
+        elif cmd in ('i', 'info'):
+            print("info [breakpoints | stack]:")
+            print("\t 'info breakpoints' will show every current breakpoint")
+            print("\t 'info stack' will show the system stack content")
+        elif cmd in ('l', 'list'):
+            print("list [start] [end]")
+            print("\t 'list' will show ten lines of code each time")
+            print("\t [start] is the start line of the code")
+            print("\t [end] is the end line of the code")
+        elif cmd in ('p', 'print'):
+            print("print [variables | variableName]:")
+            print("\t 'print variables' will print all the variables and their "
+                  "values")
+            print("\t 'print variableName' will print the variable value")
+        elif cmd in ('s', 'step'):
+            print("s, step: execute the next line and stop")
         elif cmd in ('r', 'run'):
             print("r, run: execute the current loaded program")
         else: print("unknown command: '{}'. Try 'help'.".format(cmd))
 
     def execute(self, instr):
+        PC = self.vm.PC
+        if self.step:
+            self.step = False
+            print()
+            self.vm.loader.printInstruction(instr, PC)
+            self.getCommand()
+        elif PC in self.breakpoints:
+            print()
+            self.vm.loader.printInstruction(instr, PC)
+            self.getCommand()
         self.interpreter.execute(instr)
 
     def preprocess(self):
         self.interpreter.preprocess()
 
     def ruleSelected(self, pattern, ruleNumber):
-        print('Pattern "{}" match rule: {}'.format(pattern, ruleNumber))
-        if self.breakOnRule: self.getCommand()
+        if self.breakOnRule:
+            print('\nPattern "{}" match rule: {}\n'.format(pattern, ruleNumber))
+            self.getCommand()
